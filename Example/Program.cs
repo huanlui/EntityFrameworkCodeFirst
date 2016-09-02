fbe6a8304d3f8789525d0bea6df3081becfef28a
huanlui <pombal237>
2016-09-02 16:34:40 +0200

    Adapt program to last changes

--- Example/Program.cs
+++ Example/Program.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Example.Data.Context;
-using Example.Data.Entities;
 
 namespace Example
 {
@@ -12,13 +10,13 @@ namespace Example
     {
         static void Main(string[] args)
         {
-            using (var context = new SchoolContext())
-            {
-                Student student = new Student() { StudentName = "New Student" };
+            //using (var context = new SchoolContext())
+            //{
+            //    Student student = new Student() { StudentName = "New Student" };
 
-                context.Students.Add(student);
-                context.SaveChanges();
-            }
+            //    context.Students.Add(student);
+            //    context.SaveChanges();
+            //}
         }
     }
 }
