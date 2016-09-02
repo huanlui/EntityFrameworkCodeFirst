720c31faeb44a40c6beb545cd550fcc7fa6baf1f
huanlui <pombal237>
2016-09-02 16:48:53 +0200

    Console application adds a studen each time it is executed and prints all the students present in database. IMPORTANT LEARNING: With this configuration, a LocalDB name SchoolContext es created. Easy to develop!!! We do not have to think about SQlServer by the moment. https://blogs.msdn.microsoft.com/sqlexpress/2011/10/28/localdb-where-is-my-database/

--- Example/Program.cs
+++ Example/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Example.Data;
+using Example.Data.Entities;
 
 namespace Example
 {
@@ -10,13 +12,18 @@ namespace Example
     {
         static void Main(string[] args)
         {
-            //using (var context = new SchoolContext())
-            //{
-            //    Student student = new Student() { StudentName = "New Student" };
+            using (var context = new SchoolContext())
+            {
+                Student added = new Student() { StudentName = "New Student" };
 
-            //    context.Students.Add(student);
-            //    context.SaveChanges();
-            //}
+                context.Students.Add(added);
+                context.SaveChanges();
+
+                foreach (var student in context.Students)
+                {
+                    Console.WriteLine("{0} {1}", student.StudentID, student.StudentName);
+                }
+            }
         }
     }
 }
