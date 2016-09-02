94f8eca1325dcfad5d00a1263ef495fb08611034
huanlui <pombal237>
2016-09-02 16:33:52 +0200

    Remove entities created manually and add an Entity Data Model (Empty Code First Model)

---
+++ Example/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Example.Data.Context;
+using Example.Data.Entities;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SchoolContext())
+            {
+                Student student = new Student() { StudentName = "New Student" };
+
+                context.Students.Add(student);
+                context.SaveChanges();
+            }
+        }
+    }
+}
